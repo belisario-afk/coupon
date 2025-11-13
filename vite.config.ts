08d709b46acef72d5bd5b009996ce2e209125e83
belisario-afk <belisario419mz@gmail.com>
2025-11-12 19:18:54 -0800

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  // If your repo is named 'coupon', keep this. If different, change the string accordingly.
+  base: '/coupon/',
   plugins: [react()],
   build: {
     target: 'es2018',
